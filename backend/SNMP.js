8c8385907e716c1f7a4c745b3082fa40bfc58e78
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-02-19 13:51:31 +0000

    snmp working out nicely im making it speak to a little lenovo i3 machine i installed an snmp master on and its getting temperatures

--- backend/SNMP.js
+++ backend/SNMP.js
@@ -11,7 +11,7 @@ module.exports = function(params){
 		timeout: 5000,
 		transport: "udp4",
 		trapPort: 162,
-		version: netSnmp.Version1,
+		version: netSnmp.Version2,
 		idBitsSize: 32
 	};
 	this.get = function(params){
@@ -34,6 +34,7 @@ module.exports = function(params){
 			});
 		});
 	};
+	this.getNext = getNext;
 	this.getBulk = function(params){
 		return new Promise((resolve, reject)=>{
 			const oids = params.oids, nonRepeaters = params.nonRepeaters,
@@ -63,6 +64,7 @@ module.exports = function(params){
 					 }
 					 nestedStart=nonRepeaters;
 				}
+				console.log(varbindss);
 				for(var i=nestedStart; i<varbindss.length; i++){
 					const varbinds = varbindss[i];
 					const [error, variableBindings] = parseVariableBindings(varbinds);
@@ -90,7 +92,8 @@ module.exports = function(params){
 			{
 				if(maxRepititions!==undefined)
 				{
-					session.getBulk(oids, undefined, maxRepititions, callback);
+					console.log('the one');
+					session.getBulk(oids, null, maxRepititions, callback);
 				}
 				else
 				{
@@ -99,9 +102,97 @@ module.exports = function(params){
 			}
 		});
 	};
+	this.walk = function(params){
+		return new Promise((resolve, reject)=>{
+			const feedCallback = params.feedCallback, maxRepititions = params.maxRepetitions, oid=params.oid;
+			if(!oid)throw new Error('No oid provided');
+			let variableBindingss=[];
+			const session = getSession();
+			if(maxRepititions!==undefined){
+				session.walk (oid, maxRepetitions, feedCb, doneCb);
+			}
+			else
+			{
+				session.walk (oid, feedCb, doneCb);
+			}
+			function doneCb (err) {
+				console.log('done cb');
+				if (err)
+				{
+					reject(err);
+					return;
+				}
+				resolve(feedCallback?null:variableBindingss);
+				session.close();
+			}
+
+			function feedCb (varbinds) {
+				console.log(varbinds);
+				const [err, variableBindings] = parseVariableBindings(varbinds);
+				if(err){
+					reject(err);
+					return;
+				}
+				if(feedCallback){
+					feedCallback(variableBindings);
+				}	
+				else 
+				{
+					variableBindingss.push(variableBindings);
+				}
+			}
+		});
+	};
+	this.getChildren = function(params){
+		return new Promise((resolve, reject)=>{
+			const oid = params.oid;
+			if(!oid)throw new Error('No oid provided');
+			let currentOid = oid;
+			const variableBindings=[];
+			nextOid();
+			function nextOid(){
+				getNext({oids:[currentOid], nonRepeaters:1, maxRepititions:6}).then((nextVariableBindings)=>{
+					if(nextVariableBindings.length<1){
+						resolve(variableBindings);
+						return;
+					}
+					const nextVariableBinding = nextVariableBindings[0];
+					currentOid = nextVariableBinding.getOid();
+					console.log(nextOid);
+					if(currentOid.indexOf(oid)<0){
+						resolve(variableBindings);
+						return;
+					}
+					variableBindings.push(nextVariableBinding);
+					nextOid();
+				}).catch(reject);
+			}
+		});
+	};
+	function getNext(params){
+		return new Promise((resolve, reject)=>{
+			const oids = params.oids;
+			if(!oids)throw new Error('No oids provided');
+			const session = getSession();
+			console.log(oids);
+			session.getNext(oids, function (err, varbinds) {
+				if (err) {
+					reject(err);
+					return;
+				}
+				const [error, variableBindings] = parseVariableBindings(varbinds);
+				if(error){
+					reject(error);
+					return;
+				}
+				resolve(variableBindings);
+				session.close();
+			});
+		});
+	};
 	function parseVariableBindings(varbinds, nonRepeaters){
+		console.log(varbinds);
 		const variableBindings = [], len=nonRepeaters?nonRepeaters:varbinds.length;
-			console.log(varbinds);
 		for (var i = 0; i < len; i++){
 			const variableBinding = varbinds[i];
 			if (netSnmp.isVarbindError(variableBinding)){
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const BodyParser = require('./BodyParser'), CORS=require('./CORS')
 SNMP=require('./SNMP');
+const LmTemperatureSensors= require('./LmTemperatureSensors');
 const app = express();
 const port = 80;
 setupPromises();
 const cors = new CORS(app);
 const bodyParser = new BodyParser(app, 2);
 const snmp = new SNMP({target:'192.168.0.33', community:'thisispublic'});
+const lmTemperatureSensors = new LmTemperatureSensors(snmp);
+lmTemperatureSensors.initialize().then(()=>{
+	
+});
+			/*snmp.getBulk({
+				oids:["1.3.6.1.4.1.211"],
+				nonRepeaters:1, 
+				maxRepititions:2, 
+				nonRepeatersIndexZero:true
+			}).then((variableBindingss)=>{
+				variableBindingss.forEach((variableBindings)=>{
+					variableBindings.forEach((variableBinding)=>{console.log(variableBinding.getDescription());});
+				});
+			}).catch(console.error);
+			//http://www.circitor.fr/Mibs/Html/L/LM-SENSORS-MIB.php#LMTempSensorsEntry
+			//http://www.circitor.fr/Mibs/Html/L/LM-SENSORS-MIB.php#LMTempSensorsEntry
+			snmp.walk({
+				oid:"1.3.6.1.4.1.211",
+				feedCallback:function(variableBindings){
+					variableBindings.forEach((variableBinding)=>{
+							console.log(variableBinding.getDescription());});
+				}
+			}).then(()=>{
+				console.log('done');
+			}).catch(console.error);
+			*/
 app.get('/handler', (req, res) =>{
 	console.log('request get');
 });
@@ -17,16 +44,7 @@ app.post('/handler', (req, res) =>{
 			res.send(JSON.stringify({type:'testResponse'}));
 			break;
 		case 'getData':
-			snmp.getBulk({
-				oids:["1.3.6.1.2.1.1.0.5", "1.3.6.1.2.1.1.0.6"],
-				nonRepeaters:1, 
-				maxRepititions:2, 
-				nonRepeatersIndexZero:true
-			}).then((variableBindingss)=>{
-				variableBindingss.forEach((variableBindings)=>{
-					variableBindings.forEach((variableBinding)=>{console.log(variableBinding.getDescription());});
-				});
-			}).catch(console.error);
+		
 			res.send(JSON.stringify({data: [Math.random() * 5, 2, 10]}));
 			break;
 	}
